5c5f514e1d87c5dd103ffb0f5d989230735db435
karajan1001 <mishanyo1001@gmail.com>
2023-04-17 19:29:30 +0800

    Expose more Classes and methods to the top level.

--- src/aiooss2/__init__.py
+++ src/aiooss2/__init__.py
@@ -6,4 +6,16 @@ Imports from this file follows the contents in `oss2``
 from oss2 import AnonymousAuth, Auth, StsAuth
 
 from .api import AioBucket, AioService
+from .http import AioSession
 from .iterators import AioBucketIterator, AioObjectIterator
+from .resumable import resumable_download, resumable_upload
+
+__all__ = [
+    "AioBucket",
+    "AioService",
+    "AioSession",
+    "AioBucketIterator",
+    "AioObjectIterator",
+    "resumable_upload",
+    "resumable_download",
+]
